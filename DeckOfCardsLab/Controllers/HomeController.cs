91fe01e488b80782c6d645ecdff1c1f5ae8bddfd
AliAlHashemi14 <alialhashemi14@gmail.com>
2022-07-26 11:40:27 -0400

    first commit

---
+++ DeckOfCardsLab/Controllers/HomeController.cs
@@ -0,0 +1,48 @@
+﻿using DeckOfCardsLab.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace DeckOfCardsLab.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+        public CardsDAL cardsDAL = new CardsDAL(); //calls new instance of api
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        public IActionResult CardDetails()
+        {
+            CardsModel result = cardsDAL.GetCards();
+            if(result.success == false || result.remaining < 5)
+            {
+                cardsDAL.ShuffleCards();
+                return View(result);
+            }
+            else
+            {
+                return View(result);
+            }
+            
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ DeckOfCardsLab/Models/CardsDAL.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json;
+using System.Net;
+
+namespace DeckOfCardsLab.Models
+{
+    public class CardsDAL
+    {
+        public CardsModel GetCards() //Adjust method name
+        {
+            //Adjust
+            //api url
+            string key = "mal5qf9c3vlp"; //this should be hidden
+            string url = $"https://deckofcardsapi.com/api/deck/{key}/draw/?count=5";
+
+            //setup request
+            HttpWebRequest request = WebRequest.CreateHttp(url);
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+
+            //save response data
+            StreamReader reader = new StreamReader(response.GetResponseStream());
+            string JSON = reader.ReadToEnd();
+
+            //Adjust
+            //Convert JSON to a C# object
+            CardsModel result = JsonConvert.DeserializeObject<CardsModel>(JSON);
+            return result;
+        }
+
+        public void ShuffleCards()
+        {
+            string key = "mal5qf9c3vlp";
+            string url = $"https://deckofcardsapi.com/api/deck/{key}/shuffle/";
+
+            HttpWebRequest request = WebRequest.CreateHttp(url);
+
+            HttpWebResponse response = (HttpWebResponse)request.GetResponse();
+        }
+    }
+}
